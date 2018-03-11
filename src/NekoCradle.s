c8528b389b391ab0cfbebcd1441c9edc82183643
Georg Ziegler <georgjziegler@gmail.com>
2018-03-11 21:01:18 +0100

    Add initialization routines

--- src/NekoCradle.s
+++ src/NekoCradle.s
@@ -19,6 +19,7 @@
 .include "CPUMacros.inc"
 .include "WRAMPointers.inc"
 .include "TileData.inc"
+.include "NekoCradleInitialization.inc"
 ;-------------------------------------------------------------------------------
 
 ;----- Assembler Directives ----------------------------------------------------
@@ -50,76 +51,24 @@
         lda #$8f                ; force v-blanking
         sta INIDISP
         stz NMITIMEN            ; disable NMI
-        jsl ClearRegisters
-        jsl ClearVRAM
-        jsl ClearCGRAM
 
-        ; Load Chess Tile Set
-        tsx                     ; save stack pointer
-        lda #$00                ; size bank
-        pha
-        pea $2000               ; size
-        lda #$01                ; word destination segment << 4
-        pha
-        lda #^ChessTileSet      ; bank source
-        pha
-        lda #>ChessTileSet      ; middle source
-        pha
-        lda #<ChessTileSet      ; low source
-        pha
-        jsl LoadTileSet
-        txs                     ; restore stack pointer to pre-call value
-        ; Chess Tile Set loaded
+        jsl ClearRegisters      ; set PPU and CPU registers to standard values
+        jsl ClearVRAM           ; write to complete V-RAM once
+        jsl ClearCGRAM          ; write to complete CG-RAM once
 
-        ; load palette
-        tsx                     ; save stack pointer
-        ldy #$0c                ; size: 12 bytes
-        phy
-        lda #$00                ; destination: $00
-        pha
-        lda #^ChessPalette      ; source: ChessPalette
-        pha
-        lda #>ChessPalette
-        pha
-        lda #<ChessPalette
-        pha
-        jsl LoadPalette
-        txs                     ; restore stack pointer
-        ; palette loaded
+        jsl InitNekoCradle      ; initalize neko cradle data
+        jsl InitVariables       ; set up WRAM variables
+        jsl ResetOAM
 
-        ; load tile map into VRAM
-        tsx                     ; save stack pointer
-        lda #$00                ; size: $00:0800
-        pha
-        pea $0800
-        pha                     ; word destination segment << 2
-        lda #^ChessTileMap      ; source address
-        pha
-        lda #>ChessTileMap
-        pha
-        lda #<ChessTileMap
-        pha
-        jsl LoadTileMap
-        txs                     ; restore stack pointer
-
-        ; set to BG Mode 1, BG2 tile size to 16 x 16
-        lda #$21
-        sta BGMODE
-        ; set BG2 base address to $1000-word
-        lda #$10                ; set BG2 Base Address
-        sta BG12NBA
-        ; set BG2 sc address to VRAM address
-        lda #$00
-        sta BG2SC
-        ; make BG2 visible
-        lda #$02
+        ; make BG2 and Objects visible
+        lda #$12
         sta TM
-        ; enable NMI, turn on automatic joypad polling
-        lda #$81
-        sta NMITIMEN
         ; release forced blanking
         lda #$0f
         sta INIDISP
+        ; enable NMI, turn on automatic joypad polling
+        lda #$81
+        sta NMITIMEN
 
         nop                     ; break point for debugger
 
